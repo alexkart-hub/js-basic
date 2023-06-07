04ff6c589f6a87a482b21e2e724439c189ffcc74
Alexander Kartavtsev <ugo-lok@yandex.ru>
2023-06-07 21:58:35 +0300

    LocalStorage

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// Добавление html на лету
+// Local storage
 'use strict';
 
-const panel = document.createElement('div');
-panel.classList.add('panel');
-panel.innerText = document.querySelector('.panel').innerText;
+localStorage.setItem('token', 'some_token');
+localStorage.setItem('token1', 'some_token_1');
+console.log(localStorage.getItem('token'));
+console.log(localStorage.getItem('token1'));
+localStorage.removeItem('token1');
+localStorage.clear();
 
-const button = document.createElement('button');
-button.setAttribute('user-id', 1);
-button.classList.add('button');
-button.innerText = 'Новая кнопка';
-console.log(button);
-
-document.querySelector('.test').appendChild(panel);
-document.querySelector('.test').appendChild(button);
