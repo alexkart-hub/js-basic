48abd1b84723b856855a4de7506bf6806745ce9e
Alexander Kartavtsev <ugo-lok@yandex.ru>
2023-06-09 17:19:48 +0300

    JSON

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-// Local storage
+// JSON
 'use strict';
 
-localStorage.setItem('token', 'some_token');
-localStorage.setItem('token1', 'some_token_1');
-console.log(localStorage.getItem('token'));
-console.log(localStorage.getItem('token1'));
-localStorage.removeItem('token1');
-localStorage.clear();
+function changeClick() {
+    const input = document.querySelector('.input').value;
+    if (!input) {
+        return;
+    }
+    document.querySelector('.panel').innerText = input;
+    document.querySelector('.input').value = '';
+    document.querySelector('.notification')
+        .setAttribute('class', 'notification notification_active');
+    const text = JSON.stringify({
+        text: input
+    });
+    localStorage.setItem('text', text);
+}
 
+function changeInput(e) {
+    if (e.code === 'Enter' || e.key === 'Enter') {
+        changeClick();
+    }
+}
+
+
+const obj = {
+    a: 1,
+    name: 'Вася',
+    color: [
+        {name: 'red', num: '#FF0000'}
+    ]
+};
+
+const json = JSON.stringify(obj);
+console.log(json);
+const objFromJson = JSON.parse(json);
+console.log(objFromJson);
+
+localStorage.setItem('text', json);
